6b643f05757b1b4746ed547f1f7e8d4e3b565c57
Ivan Nolasco <00402523@uca.edu.sv>
2025-08-18 19:30:39 -0600

    chore: add initial fragments to display list and detail

---
+++ app/src/main/java/com/cherruca/pokedex/ui/pokemonDetail/PokemonDetailFragment.kt
@@ -0,0 +1,31 @@
+package com.cherruca.pokedex.ui.pokemonDetail
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cherruca.pokedex.R
+
+class PokemonDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PokemonDetailFragment()
+    }
+
+    private val viewModel: PokemonDetailViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_pokemon_detail, container, false)
+    }
+}
---
+++ app/src/main/java/com/cherruca/pokedex/ui/pokemonList/PokemonListViewModel.kt
@@ -0,0 +1,7 @@
+package com.cherruca.pokedex.ui.pokemonList
+
+import androidx.lifecycle.ViewModel
+
+class PokemonListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
